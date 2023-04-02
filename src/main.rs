4d9011a1748cadd5406c9dc3f07a8122f7015e98
Matt Payne <Payne@MattPayne.org>
2023-04-01 20:51:09 -0500

    TS 4:01 before using the trait function

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,37 @@
+use std::ops::SubAssign;
+
 struct Sedan {
     color: String,
 }
 
 impl Sedan {
     fn new(color: String) -> Self { Self { color } }
+}
 
+impl LandCapable for Sedan {
     fn drive(&self) {
-        print!("{} Sedan is driving!", self.color);
+        println!("{} Sedan is driving!", self.color);
     }
 }
 
+struct SUV;
+impl LandCapable for SUV {
+    fn drive(&self) {
+        println!("SUV is driving!");
+    }
+}
+
+trait LandCapable {
+    fn drive(&self);
+}
+
+fn road_trip(vehicle: &impl LandCapable) {
+    vehicle.drive();
+}
 
 fn main() {
     let car = Sedan::new("Red".to_string());
     car.drive();
+    let suv = SUV;
+    suv.drive();
 }
